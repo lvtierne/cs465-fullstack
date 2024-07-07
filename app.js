f4942b560db0331ef27150a4d1885f2214341ee7
lvtierne <laynie.tierney@snhu.edu>
2024-07-07 16:47:54 -0500

    Add zip files for app and server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var travelRouter = require('./app_server/routes/travel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_servers', 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
