61f822c36abd7a17f124734d22513308a8ed963b
Laynie Tierney <136281319+lvtierne@users.noreply.github.com>
2024-07-23 14:36:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs'); 
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var  handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+// register handlebars partials (https://www.npm.js.com/package/hbs)
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
